79dffaba9a91e3267c5f24029eb5196fe9ff0c31
Yair G <yjgarizabalo@gmail.com>
2023-03-22 06:25:56 -0500

    [cifrado]add reduer

---
+++ src/useReducer.js
@@ -0,0 +1,74 @@
+import { act } from "react-dom/test-utils";
+
+const initalState = {
+	value: '',
+	error: false,
+	loading: false,
+	deleted: false,
+	confirmed: false,
+};
+
+// const reduer = {state, action} => {
+
+// };
+
+
+const reducerIf = (state, action) => {
+	if (action.type === 'ERROR') {
+		return {
+			...state,
+			error: true,
+			loading: false
+		}
+	} else if (action.type === 'CHECK') {
+		return {
+			...state,
+			loading: true
+		}
+	} else {
+		return {
+			...state
+		}
+	}
+};
+
+
+const reducerSwitch = (state, action) => {
+	switch (action.type) {
+		case 'ERROR':
+			return {
+				...state,
+				error: true,
+				loading: false
+			};
+		case 'CHECK':
+			return {
+				...state,
+				loading: true
+			};
+		default:
+			return {
+				...state
+			}
+	}
+}
+
+const reducerObject = (state) => ({
+	'ERROR': {
+		...state,
+		error: true,
+		loading: false
+	},
+	'CHECK': {
+		...state,
+		loading: true
+	}
+});
+
+const reducer = (state, action) => {
+	if (reducerObject(state)[action.type]) {
+		return reducerObject(state)[action.type];
+	} else {
+		return state
+	}
+};
